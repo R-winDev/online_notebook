9286b9b22584a12709b2dd7be6aa878a0f68e814
R-winDev <lowmrj@gmail.com>
2025-08-22 02:16:54 +0330

    add-partials

---
+++ views/partials/footer.php
@@ -0,0 +1,4 @@
+</div>
+<script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
+</body>
+</html>
---
+++ views/partials/head.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en" class="h-full bg-gray-900">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Note Book</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="h-full">
+<div class="min-h-full">
